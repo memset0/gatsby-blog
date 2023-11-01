c94771cab1ea10ddb732a867841830a1ec2fc86c
memset0 <memset0@outlook.com>
2023-11-01 21:35:29 +0800

    feat: add about page

---
+++ src/pages/about.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import Avatar from "@mui/material/Avatar";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Seo from "../components/Seo";
+import Layout from "../components/Layout";
+
+import siteMetadata from "../data/metadata";
+
+const awards = [
+  { title: "Award 1", description: "Description for Award 1" },
+  { title: "Award 2", description: "Description for Award 2" },
+  // Add more awards as needed
+];
+
+const AboutPage = () => (
+  <Layout title="关于">
+    <Card sx={{ display: "flex", alignItems: "center", m: 2, p: 2 }}>
+      <Avatar>
+        <StaticImage
+          src="../images/avatar.png"
+          alt={siteMetadata.author.name}
+        />
+      </Avatar>
+      <CardContent>
+        <Typography variant="h5">{siteMetadata.author.name}</Typography>
+        <Typography variant="body1">{siteMetadata.author.summary}</Typography>
+      </CardContent>
+    </Card>
+
+    <div>
+      {awards.map((award, index) => (
+        <Accordion key={index}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel${index + 1}a-content`}
+            id={`panel${index + 1}a-header`}
+          >
+            <Typography>{award.title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{award.description}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  </Layout>
+);
+
+export const Head = () => <Seo title="关于" />;
+
+export default AboutPage;
