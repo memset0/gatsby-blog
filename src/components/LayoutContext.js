b508163b05ba492228c7603d788ecf0c6ac54a20
memset0 <memset0@outlook.com>
2023-11-11 17:54:31 +0800

    fix: context is undefined when SSR

--- src/components/LayoutContext.js
+++ src/components/LayoutContext.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const LayoutContext = React.createContext();
+const LayoutContext = React.createContext({
+  title: "",
+  setTitle: () => {},
+});
 
 export default LayoutContext;
