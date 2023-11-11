778306759444b0b80aafb2748a4efb0ab66d371c
memset0 <memset0@outlook.com>
2023-11-11 23:43:56 +0800

    feat: set theme color

--- src/theme.js
+++ src/theme.js
@@ -5,10 +5,10 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#B481BB',
     },
     secondary: {
-      main: '#19857b',
+      main: '#77428D',
     },
     error: {
       main: red.A400,
