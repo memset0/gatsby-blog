e141b0a8ad671a7b20cb626e8f2d4998235e93a9
Memento mori <34177126+memset0@users.noreply.github.com>
2024-02-11 15:43:14 +0800

    feat(404): refactor page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import Main from "../components/Main";
 import Seo from "../components/Seo";
 
@@ -7,11 +10,21 @@ const getTitle = () => "404: Not Found";
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title;
-
+  console.log("[404]", siteTitle);
+  
   return (
-    <Main maxWidth="sm" title={getTitle()} location={location}>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Main maxWidth="md" title={getTitle()} location={location}>
+      <Card sx={{ mt: 1 }}>
+        <CardContent sx={{ pl: 3, pr: 3 }}>
+          <Typography variant="h3" sx={{ mt: 4, mb: 2 }}>
+            404: Not Found
+          </Typography>
+          <Typography variant="body1">
+            <p>非常抱歉，你访问的页面并不存在……</p>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          </Typography>
+        </CardContent>
+      </Card>
     </Main>
   );
 };
