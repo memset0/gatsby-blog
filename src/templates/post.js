477147ac2621ccfea2cd183c29b7a2979c798dfc
memset0 <memset0@outlook.com>
2024-02-13 20:59:12 +0800

    feat(post): refactor style of date & category

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link as GatsbyLink } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Card from "@mui/material/Card";
@@ -8,12 +8,12 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import EventIcon from "@mui/icons-material/Event";
-import CategoryIcon from "@mui/icons-material/Category";
 import Seo from "../components/Seo";
 import Main from "../components/Main";
 import Link from "../components/Link";
 import Comments from "../components/Comments";
+import CalendarAltIcon from "../components/Icon/CalendarAltIcon";
+import LayerGroupIcon from "../components/Icon/LayerGroupIcon";
 import TableOfContents from "../components/TableOfContents";
 
 import { checkNegIndent } from "../utils/frontend";
@@ -27,9 +27,11 @@ const PostTemplate = ({ data, pageContext, location }) => {
   const { post } = data;
   const { navJson } = pageContext;
   const { slug, isDoc, cover, propsJson } = post.fields;
+  const { date } = post.frontmatter;
 
   const coverImage = cover && getImage(cover);
   const props = propsJson && JSON.parse(propsJson);
+  const category = !isDoc && JSON.parse(post.fields.category);
 
   // console.log("[nav]", navJson && JSON.parse(navJson));
   // console.log("[props]", props);
@@ -59,70 +61,74 @@ const PostTemplate = ({ data, pageContext, location }) => {
                 }}
               >
                 {isDoc ? (
-                  <>
-                    <Typography
-                      component="div"
-                      className={styles.articleTitle + " " + styles.articleTitleDoc}
-                      sx={{
-                        textIndent: checkNegIndent(post.frontmatter.title) ? "-0.32em" : 0,
-                        mt: 2,
-                      }}
-                    >
-                      {post.frontmatter.title}
-                    </Typography>
-
-                    {props && (
-                      <Box sx={{ mt: 2, ml: 4 }}>
-                        {props.map((el, index) => (
-                          <Typography
-                            variant="body1"
-                            component="div"
-                            key={index}
-                            sx={{ lineHeight: "1.8", color: "grey" }}
-                          >
-                            <div style={{ width: "9.8em", display: "inline-block" }}>{el.key}</div>
-                            {el.value}
-                          </Typography>
-                        ))}
-                      </Box>
-                    )}
-                  </>
+                  <Typography
+                    component="div"
+                    className={styles.articleTitle + " " + styles.articleTitleDoc}
+                    sx={{
+                      textIndent: checkNegIndent(post.frontmatter.title) ? "-0.32em" : 0,
+                      mt: 2,
+                    }}
+                  >
+                    {post.frontmatter.title}
+                  </Typography>
                 ) : (
-                  <>
-                    <Typography
-                      component="div"
-                      className={styles.articleTitle + " " + styles.articleTitlePost}
-                      sx={{ textIndent: checkNegIndent(post.frontmatter.title) ? "-0.32em" : 0 }}
-                    >
-                      {post.frontmatter.title}
-                    </Typography>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        color: "grey.500",
-                        mt: 1.5,
-                        mb: 4,
-                      }}
-                    >
-                      <EventIcon sx={{ mr: 1 }} />
-                      <Typography variant="body1" color="inherit">
-                        {post.frontmatter.date}
-                      </Typography>
-
-                      <CategoryIcon sx={{ ml: 2, mr: 1 }} />
-                      <Breadcrumbs aria-label="breadcrumb" sx={{ color: "grey.500" }}>
-                        {JSON.parse(post.fields.category).map(({ name, to }, index) => (
-                          <Link underline="hover" color="inherit" href={to} key={index}>
-                            {name}
-                          </Link>
-                        ))}
-                      </Breadcrumbs>
-                    </Box>
-                  </>
+                  <Typography
+                    component="div"
+                    className={styles.articleTitle + " " + styles.articleTitlePost}
+                    sx={{ textIndent: checkNegIndent(post.frontmatter.title) ? "-0.32em" : 0 }}
+                  >
+                    {post.frontmatter.title}
+                  </Typography>
                 )}
 
-                <Typography variant="body1" sx={{ mt: 2 }}>
+                {(date || category) && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      color: "grey.500",
+                      mt: 1,
+                      mb: 4,
+                      "& svg": { ml: 2.25, mr: 1.25 },
+                      "& :first-child": { ml: 0 },
+                    }}
+                  >
+                    <LayerGroupIcon fontSize="small" />
+                    <Breadcrumbs
+                      aria-label="breadcrumb"
+                      sx={{ color: "inherit", "& .MuiBreadcrumbs-separator": { mx: 0.75 } }}
+                    >
+                      {category.map(({ name, to }, index) => (
+                        <Link component={GatsbyLink} underline="hover" color="inherit" to={to} key={index}>
+                          {name}
+                        </Link>
+                      ))}
+                    </Breadcrumbs>
+
+                    <CalendarAltIcon fontSize="small" />
+                    <Typography variant="body1" color="inherit" sx={{ wordSpacing: "-2.5px" }}>
+                      {date}
+                    </Typography>
+                  </Box>
+                )}
+
+                {props && (
+                  <Box sx={{ mt: 2, ml: 4 }}>
+                    {props.map((el, index) => (
+                      <Typography
+                        variant="body1"
+                        component="div"
+                        key={index}
+                        sx={{ lineHeight: "1.8", color: "grey" }}
+                      >
+                        <div style={{ width: "9.8em", display: "inline-block" }}>{el.key}</div>
+                        {el.value}
+                      </Typography>
+                    ))}
+                  </Box>
+                )}
+
+                <Typography variant="body1" sx={{ mt: 4 }}>
                   <section
                     className="typography"
                     itemProp="articleBody"
@@ -200,7 +206,7 @@ export const pageQuery = graphql`
       tableOfContents(maxDepth: 3)
       frontmatter {
         title
-        date(formatString: "MM/DD/YYYY")
+        date(formatString: "YYYY 年 MM 月 DD 日")
         description
       }
       fields {
