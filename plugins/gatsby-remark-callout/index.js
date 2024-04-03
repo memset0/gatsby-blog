e60e66a0994f6504aadfc188ccc37ef6b9f36bb2
memset0 <memset0@outlook.com>
2024-04-03 22:17:16 +0800

    fix(callout): some type declaration not removed

--- plugins/gatsby-remark-callout/index.js
+++ plugins/gatsby-remark-callout/index.js
@@ -31,11 +31,6 @@ module.exports = ({ markdownAST }) => {
       heading = node.children[0].children.slice(0, headingEndBlock);
       heading.push(JSON.parse(JSON.stringify(node.children[0].children[headingEndBlock]))); // 最后一个 section 要 clone，以免在下面被修改
       heading[heading.length - 1].value = heading[heading.length - 1].value.slice(0, headingEndPos);
-      heading[0].value = heading[0].value.slice(type.length + 3); // +3 是因为考虑到多了 ![] 三个字符
-      if (collapse) {
-        // 如果是折叠的话，还要在 +1，因为有字符 -
-        heading[0].value = heading[0].value.slice(1);
-      }
       if (heading.length === 1 && heading[0].type === "text" && heading[0].value.trim() === "") {
         // 如果没有指定 Callout 的标题，则根据类型自动生成
         heading[0].value = type[0].toUpperCase() + type.slice(1);
@@ -44,6 +39,13 @@ module.exports = ({ markdownAST }) => {
       // 从中删掉标题部分
       node.children[0].children = node.children[0].children.slice(headingEndBlock);
       node.children[0].children[0].value = node.children[0].children[0].value.slice(headingEndPos);
+    }
+
+    // 从标题中删掉 callout 的 type 声明
+    heading[0].value = heading[0].value.slice(type.length + 3); // +3 是因为考虑到多了 ![] 三个字符
+    if (collapse) {
+      // 如果是折叠的话，还要在 +1，因为有字符 -
+      heading[0].value = heading[0].value.slice(1);
     }
 
     node.type = "paragraph";
