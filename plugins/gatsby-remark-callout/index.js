6509837a97c94ee3c73b6bc3c11fdc4bd6ac91df
memset0 <memset0@outlook.com>
2024-04-03 21:45:50 +0800

    fix(callout): may crash in some situation

--- plugins/gatsby-remark-callout/index.js
+++ plugins/gatsby-remark-callout/index.js
@@ -1,17 +1,7 @@
 const visit = require("unist-util-visit");
 
 module.exports = ({ markdownAST }) => {
-  visit(markdownAST, "blockquote", node => {
-    if (
-      !(
-        node.children[0].type == "paragraph" &&
-        node.children[0].children[0].type == "text" &&
-        node.children[0].children[0].value.startsWith("[!")
-      )
-    ) {
-      return;
-    }
-
+  function transformToCallout(node) {
     const target = node.children[0].children[0].value;
     const type = target.slice(2, target.indexOf("]"));
     let collapse = false;
@@ -31,28 +21,30 @@ module.exports = ({ markdownAST }) => {
         break;
       }
     }
+
+    let heading;
     if (headingEndBlock == -1 && headingEndPos == -1) {
-      headingEndBlock = node.children[0].children.length - 1;
-      headingEndPos = node.children[0].children[headingEndBlock].value.length;
-    }
+      heading = node.children[0].children;
+      node.children[0].children = [];
+    } else {
+      // 从中提取出标题部分
+      heading = node.children[0].children.slice(0, headingEndBlock);
+      heading.push(JSON.parse(JSON.stringify(node.children[0].children[headingEndBlock]))); // 最后一个 section 要 clone，以免在下面被修改
+      heading[heading.length - 1].value = heading[heading.length - 1].value.slice(0, headingEndPos);
+      heading[0].value = heading[0].value.slice(type.length + 3); // +3 是因为考虑到多了 ![] 三个字符
+      if (collapse) {
+        // 如果是折叠的话，还要在 +1，因为有字符 -
+        heading[0].value = heading[0].value.slice(1);
+      }
+      if (heading.length === 1 && heading[0].type === "text" && heading[0].value.trim() === "") {
+        // 如果没有指定 Callout 的标题，则根据类型自动生成
+        heading[0].value = type[0].toUpperCase() + type.slice(1);
+      }
 
-    // 从中提取出标题部分
-    const heading = node.children[0].children.slice(0, headingEndBlock);
-    heading.push(JSON.parse(JSON.stringify(node.children[0].children[headingEndBlock]))); // 最后一个 section 要 clone，以免在下面被修改
-    heading[heading.length - 1].value = heading[heading.length - 1].value.slice(0, headingEndPos);
-    heading[0].value = heading[0].value.slice(type.length + 3); // +3 是因为考虑到多了 ![] 三个字符
-    if (collapse) {
-      // 如果是折叠的话，还要在 +1，因为有字符 -
-      heading[0].value = heading[0].value.slice(1);
+      // 从中删掉标题部分
+      node.children[0].children = node.children[0].children.slice(headingEndBlock);
+      node.children[0].children[0].value = node.children[0].children[0].value.slice(headingEndPos);
     }
-    if (heading.length === 1 && heading[0].type === "text" && heading[0].value.trim() === "") {
-      // 如果没有指定 Callout 的标题，则根据类型自动生成
-      heading[0].value = type[0].toUpperCase() + type.slice(1);
-    }
-
-    // 从中删掉标题部分
-    node.children[0].children = node.children[0].children.slice(headingEndBlock);
-    node.children[0].children[0].value = node.children[0].children[0].value.slice(headingEndPos);
 
     node.type = "paragraph";
     node.children.unshift({
@@ -70,5 +62,24 @@ module.exports = ({ markdownAST }) => {
       type: "html",
       value: "</div></details>",
     });
+  }
+
+  visit(markdownAST, "blockquote", node => {
+    if (
+      !(
+        node.children[0].type == "paragraph" &&
+        node.children[0].children[0].type == "text" &&
+        node.children[0].children[0].value.startsWith("[!")
+      )
+    ) {
+      return;
+    }
+
+    try {
+      transformToCallout(node);
+    } catch (error) {
+      console.error("[callout] build failed:", error, JSON.stringify(node, null, 2));
+      throw error;
+    }
   });
 };
