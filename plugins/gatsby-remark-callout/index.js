e4e4f5baf52d189396399eb49582356b07541cdb
memset0 <memset0@outlook.com>
2024-04-03 21:21:24 +0800

    feat(callout): add support for complex format titles

--- plugins/gatsby-remark-callout/index.js
+++ plugins/gatsby-remark-callout/index.js
@@ -13,36 +13,62 @@ module.exports = ({ markdownAST }) => {
     }
 
     const target = node.children[0].children[0].value;
-    const heading = target.slice(0, target.indexOf("\n") == -1 ? target.length : target.indexOf("\n"));
-
-    const type = heading.slice(2, heading.indexOf("]"));
-    // if (!Object.keys(config.theme).includes(type)) {
-    //   return;
-    // }
-
-    let title = heading.slice(heading.indexOf("]") + 1).trim();
+    const type = target.slice(2, target.indexOf("]"));
     let collapse = false;
-    if (title.startsWith("-")) {
+    if (target[target.indexOf("]") + 1] === "-") {
       collapse = true;
-      title = title.slice(1).trim();
-    }
-    if (!title) {
-      title = type[0].toUpperCase() + type.slice(1);
     }
 
-    node.children[0].children[0].value = node.children[0].children[0].value.slice(heading.length);
+    let headingEndBlock = -1;
+    let headingEndPos = -1;
+    for (let i = 0; i < node.children[0].children.length; i++) {
+      if (
+        node.children[0].children[i].type == "text" &&
+        node.children[0].children[i].value.indexOf("\n") !== -1
+      ) {
+        headingEndBlock = i;
+        headingEndPos = node.children[0].children[i].value.indexOf("\n");
+        break;
+      }
+    }
+    if (headingEndBlock == -1 && headingEndPos == -1) {
+      headingEndBlock = node.children[0].children.length - 1;
+      headingEndPos = node.children[0].children[headingEndBlock].value.length;
+    }
+
+    // 从中提取出标题部分
+    const heading = node.children[0].children.slice(0, headingEndBlock);
+    heading.push(JSON.parse(JSON.stringify(node.children[0].children[headingEndBlock]))); // 最后一个 section 要 clone，以免在下面被修改
+    heading[heading.length - 1].value = heading[heading.length - 1].value.slice(0, headingEndPos);
+    heading[0].value = heading[0].value.slice(type.length + 3); // +3 是因为考虑到多了 ![] 三个字符
+    if (collapse) {
+      // 如果是折叠的话，还要在 +1，因为有字符 -
+      heading[0].value = heading[0].value.slice(1);
+    }
+    if (heading.length === 1 && heading[0].type === "text" && heading[0].value.trim() === "") {
+      // 如果没有指定 Callout 的标题，则根据类型自动生成
+      heading[0].value = type[0].toUpperCase() + type.slice(1);
+    }
+
+    // 从中删掉标题部分
+    node.children[0].children = node.children[0].children.slice(headingEndBlock);
+    node.children[0].children[0].value = node.children[0].children[0].value.slice(headingEndPos);
+
     node.type = "paragraph";
     node.children.unshift({
       type: "html",
-      value: `<details ${
-        collapse ? "" : "open disabled"
-      } class="callout callout-type-${type}"><summary>${title}</summary><div>`,
+      value: `</summary><div>`,
+    });
+    for (let i = heading.length - 1; i >= 0; i--) {
+      node.children.unshift(heading[i]);
+    }
+    node.children.unshift({
+      type: "html",
+      value: `<details ${collapse ? "" : "open disabled"} class="callout callout-type-${type}"><summary>`,
     });
     node.children.push({
       type: "html",
       value: "</div></details>",
     });
-    // console.log(node.children);
-    // console.log(">note", "!!!", heading, type, title);
   });
 };
