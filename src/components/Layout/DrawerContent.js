7f2e5393e4965b092d06fcbc1d6762d5d8a4326f
memset0 <memset0@outlook.com>
2024-09-15 23:53:27 +0800

    refactor: move data of navigators to another file

--- src/components/Layout/DrawerContent.js
+++ src/components/Layout/DrawerContent.js
@@ -9,50 +9,13 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
 import Avatar from "@mui/material/Avatar";
-import HomeIcon from "@mui/icons-material/Home";
-import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
-import PersonIcon from "@mui/icons-material/Person";
-import LeaderboardIcon from "@mui/icons-material/Leaderboard";
-import LinkIcon from "@mui/icons-material/Link";
 import { Link as GatsbyLink } from "gatsby";
 
 import siteMetadata from "../../data/metadata";
 import theme from "../../theme";
+import navigators from "../../data/navigators";
 
 const DrawerContent = ({ fold, pathname }) => {
-  const navigators = [
-    {
-      to: "/",
-      text: "所有文章",
-      icon: <HomeIcon />,
-      rule: pathname => pathname === "/",
-    },
-    {
-      to: "/oi/",
-      text: "算法竞赛",
-      icon: <LeaderboardIcon />,
-      rule: pathname => pathname.startsWith("/oi/"),
-    },
-    {
-      to: "/course/",
-      text: "课程笔记",
-      icon: <LocalLibraryIcon />,
-      rule: pathname => pathname.startsWith("/course/"),
-    },
-    {
-      to: "/friends/",
-      text: "友情链接",
-      icon: <LinkIcon />,
-      rule: pathname => pathname === "/friends/",
-    },
-    {
-      to: "/about/",
-      text: "关于博主",
-      icon: <PersonIcon />,
-      rule: pathname => pathname === "/about/",
-    },
-  ];
-
   return (
     <>
       <Toolbar
---
+++ src/data/navigators.js
@@ -0,0 +1,39 @@
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import PersonIcon from "@mui/icons-material/Person";
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
+import LinkIcon from "@mui/icons-material/Link";
+
+export default [
+  {
+    to: "/",
+    text: "所有文章",
+    icon: <HomeIcon />,
+    rule: pathname => pathname === "/",
+  },
+  {
+    to: "/oi/",
+    text: "算法竞赛",
+    icon: <LeaderboardIcon />,
+    rule: pathname => pathname.startsWith("/oi/"),
+  },
+  {
+    to: "/course/",
+    text: "课程笔记",
+    icon: <LocalLibraryIcon />,
+    rule: pathname => pathname.startsWith("/course/"),
+  },
+  {
+    to: "/friends/",
+    text: "友情链接",
+    icon: <LinkIcon />,
+    rule: pathname => pathname === "/friends/",
+  },
+  {
+    to: "/about/",
+    text: "关于博主",
+    icon: <PersonIcon />,
+    rule: pathname => pathname === "/about/",
+  },
+];
